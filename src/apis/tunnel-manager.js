fa00f43a3643fc6c92964647437eace9d9496cbf
Mikhail Semin <bifot@yandex.ru>
2019-10-27 13:25:55 +0300

    removed redis

--- src/apis/tunnel-manager.js
+++ src/apis/tunnel-manager.js
@@ -1,46 +1,8 @@
 const generate = require('nanoid/generate');
-const redis = require('./redis');
-const Tunnel = require('./tunnel');
-const TunnelAgent = require('./tunnel-agent');
-const { safeParse } = require('../helpers');
 
 class TunnelManager {
   constructor() {
     this.tunnels = {};
-
-    this.startup();
-  }
-
-  async startup() {
-    const keys = await redis.hgetall('tunnels');
-
-    if (!keys) {
-      return;
-    }
-
-    await Promise.all(
-      Object.entries(keys).map(async ([key, data]) => {
-        const { name, port } = safeParse(data);
-
-        if (!name || !port) {
-          redis.hdel('tunnels', key)
-            .catch(() => undefined);
-
-          return;
-        }
-
-        const agent = new TunnelAgent(port);
-        const tunnel = new Tunnel(name, agent);
-
-        agent.on('dead', () => {
-          this.remove(name);
-        });
-
-        await agent.start();
-
-        this.tunnels[name] = tunnel;
-      }),
-    );
   }
 
   getAvailableName() {
@@ -52,29 +14,10 @@ class TunnelManager {
   }
 
   add(name, tunnel) {
-    setImmediate(async () => {
-      try {
-        await redis.hset('tunnels', name, JSON.stringify({
-          name,
-          port: tunnel.agent.port,
-        }));
-      } catch (err) {
-        console.error('Cannot save tunnel settings into redis', err);
-      }
-    });
-
     this.tunnels[name] = tunnel;
   }
 
   remove(name) {
-    setImmediate(async () => {
-      try {
-        await redis.hdel('tunnels', name);
-      } catch (err) {
-        console.error('Cannot delete tunnel settings from redis', err);
-      }
-    });
-
     delete this.tunnels[name];
   }
 
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,5 @@
-const safeParse = require('./safeParse');
 const parseQuery = require('./parseQuery');
 
 module.exports = {
-  safeParse,
   parseQuery,
 };
